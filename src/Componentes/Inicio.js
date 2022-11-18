3c13cf752bf49eb11bab90dd6e2c10cd8f7e9b2a
Eduard1401 <eduardjimenez8916@gmail.com>
2022-11-18 18:45:53 -0500

    Actualización página de Inicio
    Se actualiza el código completo de la página de Inicio.

--- src/Componentes/Inicio.js
+++ src/Componentes/Inicio.js
@@ -12,17 +12,17 @@ import afiliaciones from '../Imagenes/afiliaciones.png'
 
 export const Inicio = () => {
   return (
-    <div className="App"> 
+  <div className="App"> 
     <header className="App-header">
       <img className="logo" src={logo_clinica} alt='' />
       <img className="nombre" src={nombre_clinica} alt='' />
     </header>
     <body className='body'>
         <div className="rectangulo"></div>
-          <button className="boton">Oficina virtual afiliados</button>
+        <button className="boton">Oficina virtual afiliados</button>
         <button className="boton2">Registrate afiliado</button>
         <form method='get'action={Inicio}>
-            <button type='submit'className="boton3">Inicio</button>
+          <button type='submit'className="boton3">Inicio</button>
         </form>
         <button className="boton4">Red de Atención</button>
         <button className="boton5"> Afiliados</button>
@@ -54,8 +54,6 @@ export const Inicio = () => {
         <img className="afiliaciones" src={afiliaciones} alt='' />
         <texto className="afiliaciones1"><b>Afiliacion</b></texto>
     </body>
-
-
   </div>
   )
 }
